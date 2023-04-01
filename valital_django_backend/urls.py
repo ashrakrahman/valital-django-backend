d58d758a4e45af58e39c827325827310ee6e8ba4
ashrak rahman lipu <ashrakrahman@gmail.com>
2023-03-31 23:56:01 -0400

    Integrate words app url and add swagger url to the root url

--- valital_django_backend/urls.py
+++ valital_django_backend/urls.py
@@ -14,8 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Valital Django API",
+        default_version="v1",
+        description="Technical Test",
+        terms_of_service="N/A",
+        contact=openapi.Contact(email="ashrakragman@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    # For local development change it to local port, for docker it should be docker port
+    url="http://localhost:8000",
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("words/", include("words.urls")),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
